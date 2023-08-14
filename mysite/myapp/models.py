47fa73c6dbf30959739bce7a2f3f1b146b2d83ac
nandan <nandan.singhs007@gmail.com>
2023-08-14 21:36:57 +0545

    adding functionalities

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    category = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)                  # Add the Date without manually entering
+    
+    def __str__(self):
+        return self.name
+    
