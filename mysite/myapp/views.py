c5f968e160c5cf8a9d759e4ebb8e22d5403841ab
nandan <nandan.singhs007@gmail.com>
2023-08-14 22:32:45 +0545

    Getting Expenses

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import ExpenseForm
+from .models import Expense
 
 # Create your views here.
 def index(request):
@@ -8,5 +9,6 @@ def index(request):
         if expense.is_valid():
             expense.save()
             
+    expenses = Expense.objects.all()
     expense_form = ExpenseForm()
-    return render(request,'myapp/index.html',{'expense_form':expense_form})
+    return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses})
