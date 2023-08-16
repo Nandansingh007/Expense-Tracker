1c687117ff4b90f4223bbb7e4a8801626af8d890
nandan <nandan.singhs007@gmail.com>
2023-08-16 15:32:45 +0545

    Adding Yearly Monthly and Weekly Sum of Expenses

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import ExpenseForm
 from .models import Expense
+import datetime
 from django.db.models import Sum
 
 # Create your views here.
@@ -14,10 +15,24 @@ def index(request):
             
     expenses = Expense.objects.all()
     total_expenses = expenses.aggregate(Sum('amount'))
-    print(total_expenses)
+   
+    #Logic to calculate 365 days expenses
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    data = Expense.objects.filter(date__gt=last_year)
+    yearly_sum = data.aggregate(Sum('amount'))
     
+    #Logic to calculate 30 days expenses
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    data = Expense.objects.filter(date__gt=last_month)
+    monthly_sum = data.aggregate(Sum('amount'))
+    
+    #Logic to calculate 7 days expenses
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    data = Expense.objects.filter(date__gt=last_week)
+    weekly_sum = data.aggregate(Sum('amount'))
+     
     expense_form = ExpenseForm()
-    return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses, 'total_expenses':total_expenses})
+    return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses, 'total_expenses':total_expenses,'yearly_sum':yearly_sum,'monthly_sum':monthly_sum,'weekly_sum':weekly_sum})
 
 #Function to edit the expenses
 def edit(request,id):
